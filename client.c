e5b10c41b8f72c4b6268bbc7b0a3aec8a6cff131
tomasz-pucka <tomasz.pucka@student.put.poznan.pl>
2018-12-24 03:19:21 +0100

    support room-communication

--- client.c
+++ client.c
@@ -1,32 +1,48 @@
-	#include <pthread.h>
-	#include <sys/types.h>
-	#include <sys/socket.h>
-	#include <netinet/in.h>
-	#include <netdb.h>
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <string.h>
-	#include <unistd.h>
-	#include <stdio.h>
-	#include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 
-	int main() {
-		char s_out[100];
-		int connection_socket_descriptor;
-		struct sockaddr_in server_address;
+#define BUF_SIZE 4096
 
-		memset(&server_address, 0, sizeof(struct sockaddr));
-		server_address.sin_family = AF_INET;
-		inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
-		server_address.sin_port = htons(1234);
-		connection_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-		connect(connection_socket_descriptor, (struct sockaddr*)&server_address, sizeof(server_address));
-		
-		while(1) {
-			scanf("%s", s_out);
-			send(connection_socket_descriptor, s_out, 100, 0);
-			if(!strcmp(s_out, "//exit")) break;
-		}
-		close(connection_socket_descriptor);
-		return 0;
+int connection_socket_descriptor;
+char text_buf[BUF_SIZE];
+char in_buf[BUF_SIZE];
+
+void *printIncomingMessagesThread() {
+	while(1){
+		recv(connection_socket_descriptor, in_buf, BUF_SIZE, 0);
+		printf("%s\n", in_buf);
 	}
+}
+
+int main() {
+	pthread_t thread;
+	struct sockaddr_in server_address;
+
+	memset(&server_address, 0, sizeof(struct sockaddr));
+	server_address.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
+	server_address.sin_port = htons(1234);
+	connection_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+	connect(connection_socket_descriptor, (struct sockaddr*)&server_address, sizeof(server_address));
+	system("clear");
+	puts("type room;nick");
+	scanf("%s", text_buf);
+	send(connection_socket_descriptor, text_buf, BUF_SIZE, 0); // room;nick
+	pthread_create(&thread, NULL, printIncomingMessagesThread, NULL);
+	while(1) {
+		scanf("%s", text_buf);
+		send(connection_socket_descriptor, text_buf, BUF_SIZE, 0);
+		if(!strcmp(text_buf, ";;exit")) break;
+	}
+	close(connection_socket_descriptor);
+	return 0;
+}
